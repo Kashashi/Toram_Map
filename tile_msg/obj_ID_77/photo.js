9f9c65dab7d96ed76f852ad9cab0ffa41e2df1b2
Noyryk <38377804+Noyryk@users.noreply.github.com>
2019-02-04 14:20:05 +0800

    Add files via upload

---
+++ tile_msg/obj_ID_77/photo.js
@@ -0,0 +1,7 @@
+function display(photos){   
+var traget=document.getElementById(photos);   
+    if(traget.style.display=="none")  
+    { traget.style.display="";  
+    }else{ traget.style.display="none";   
+    }   
+}  
